ce75b7cde553bed21d6c0085a01f9d376c989014
xyk0058 <13301007@bjtu.edu.cn>
2016-03-15 10:36:41 +0800

    xyk

---
+++ src/com/dhcc/model/U_Type.java
@@ -0,0 +1,19 @@
+package com.dhcc.model;
+
+public class U_Type {
+	double value;
+	int j;
+	
+	public double getValue() {
+		return value;
+	}
+	public void setValue(double value) {
+		this.value = value;
+	}
+	public int getJ() {
+		return j;
+	}
+	public void setJ(int j) {
+		this.j = j;
+	}
+}
